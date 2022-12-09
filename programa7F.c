f86d01d1754ca814e225ff57385737f0b3affd17
tobi-ttdy-111 <104658114+tobi-ttdy-111@users.noreply.github.com>
2022-12-09 16:57:13 -0600

    Update programa7F.c

--- programa7F.c
+++ programa7F.c
@@ -1,25 +1,35 @@
-#include <stdio.h>
-#include <stdlib.h>
-void main() {
-    int longitudArray;
-    printf("\nLongitud matriz: ");
-    scanf("%i", &longitudArray);
-    int numeroRelleno;
-    printf("Numero principal: ");
-    scanf("%i", &numeroRelleno);
-    int arreglo[longitudArray][longitudArray];
-    for (int i = 0; i < longitudArray; i++)
-    {
-        for (int j = 0; j < longitudArray; j++) {
-            arreglo[i][j] = numeroRelleno;
+# include <stdio.h>
+# include <stdlib.h>
+
+
+void main (){
+
+    int longitudFilas  = 0, longitudColumnas = 0;
+    char ** puntero;
+    char caracterLlenado = '*';
+    char dimension;
+
+    printf( "\nNumero longitudFilas y columnas: ");
+    scanf( "%i", &longitudFilas );
+    longitudColumnas = longitudFilas ;
+    printf("\nCaracter de llenado: ");
+    scanf( "%s", &caracterLlenado );
+
+    puntero = malloc( longitudFilas * ( sizeof( char* ) ) );
+    for ( int i = 0; i < longitudFilas ; i++ ) {
+        dimension = i+'0';
+        * ( puntero + i ) = malloc( longitudColumnas * ( sizeof( char ) ) );
+        for ( int j = 0; j < longitudColumnas; j++ ) {
+            if ( i == j ) *(*(puntero+i)+j) = dimension; else *( *(puntero+i)+j ) = caracterLlenado;
         };
-        arreglo[i][i] = i;
     };
-    for (int i = 0; i < longitudArray; i++)
-    {
-        printf("\n");
-        for (int j = 0; j < longitudArray; j++) {
-            printf("%i   ", arreglo[i][j]);
-        };
+
+    for ( int i = 0; i < longitudFilas ; i++ ) {
+        printf("[");
+        for (int j = 0; j < longitudColumnas; j++) { printf(" %c ", *(*(puntero+i)+j)); };
+        printf("]\n");
     };
+
+    for (int i = 0; i < longitudFilas ; i++) {  free(*(puntero+i)); };
+
 };
